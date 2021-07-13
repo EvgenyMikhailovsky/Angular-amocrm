30aed7a1a192602a30d4e6f3a81e490bcd11e7ff
reymons <reymonsma@gmail.com>
2021-07-13 15:18:20 +0500

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormModule } from './components/form/form.module';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
